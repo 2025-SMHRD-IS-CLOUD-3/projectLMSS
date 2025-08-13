7cf76681ac9523a7bc8a9af672a47bf2b6ad4272
smhrd <smhrd@192.168.219.248>
2025-08-13 15:25:39 +0900

    필터 구현

---
+++ AI_Landmark_Service/src/main/java/controller/CorsFilter.java
@@ -0,0 +1,31 @@
+package controller;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+@WebFilter("/*") // 모든 요청("/*")에 대해 이 필터를 적용합니다.
+public class CorsFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        // 여기에 프론트엔드 서버의 주소를 정확히 적어주세요.
+        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {}
+    public void destroy() {}
+}
