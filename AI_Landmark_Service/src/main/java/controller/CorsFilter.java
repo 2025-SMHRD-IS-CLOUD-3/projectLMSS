22c91fab3941db6a1c54ba2d069c3df0da8ed035
smhrd <smhrd@192.168.219.248>
2025-08-14 14:02:44 +0900

    Revert "jakarta -> javax"
    This reverts commit 6fe02f613d13a43954d8d1a9dfbb495081f616c9.

--- AI_Landmark_Service/src/main/java/controller/CorsFilter.java
+++ AI_Landmark_Service/src/main/java/controller/CorsFilter.java
@@ -1,5 +1,4 @@
 package controller;
-
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,37 +8,25 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
-
-
 @WebFilter("/*") // 모든 요청("/*")에 대해 이 필터를 적용합니다.
 public class CorsFilter implements Filter {
 
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
     	
     	System.out.println("====== CORS 필터 실행됨! ======"); 
         HttpServletResponse response = (HttpServletResponse) res;
-        
+
         // 여기에 프론트엔드 서버의 주소를 정확히 적어주세요.
-        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+        response.setHeader("Access-Control-Allow-Origin", "*");
 
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
 
-//        // 프리플라이트 요청일 경우 바로 200 응답 후 종료
-//        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-//            response.setStatus(HttpServletResponse.SC_OK);
-//            return;
-//        }
-        
-        
         chain.doFilter(req, res);
     }
 
-	public void init(FilterConfig filterConfig) {
-	}
-
-	public void destroy() {
-	}
+    public void init(FilterConfig filterConfig) {}
+    public void destroy() {}
 }
